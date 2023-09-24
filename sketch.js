5381a238ea07538650322d9164522580c129a941
notstupid1 <143924946+notstupid1@users.noreply.github.com>
2023-09-24 16:58:20 +0530

    Added logic for randomizing color

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 const sketchPad = document.querySelector('.sketch-page');
 const resetBtn = document.querySelector('#reset');
 const newSketchBtn = document.querySelector('#new-sketch');
+const randomizeBtn = document.querySelector('#randomize');
+
+let isRandomized = false;
 
 let defaultSize = 16;
+let defaultColor = '#eaeaea';
 
 let sketchPadPixel = [];
 
@@ -23,27 +27,26 @@ function createSketchPad(size){
             sketchPadPixel[j] = document.createElement('div');   
             sketchPadPixel[j].style.height = `${pixelSize}px`;
             sketchPadPixel[j].style.width = `${pixelSize}px`;
-            render(sketchPadPixel[j], '#d6d6d6');
+            render(sketchPadPixel[j]);
             sketchPad.appendChild(sketchPadPixel[j]);  
         }
     }
-    console.log(sketchPadPixel.length);
-    console.log(sketchPadPixel);
 }
 
-function render(pixel, color) {
+function render(pixel, color = defaultColor) {
     pixel.style.backgroundColor = color;
 }
 
 function draw() {
     for (let pixel of sketchPadPixel) {
-        pixel.addEventListener('mouseover', () => render(pixel, 'black'));
+        pixel.addEventListener('mouseover', 
+        () => (!isRandomized) ? render(pixel, 'black') : render(pixel, randomize()));
     }
 }
 
 function reset() {
     for (let pixel of sketchPadPixel) {
-        render(pixel, '#d6d6d6');
+        render(pixel);
     }
 }
 
@@ -60,6 +63,26 @@ function newSketch() {
     createSketchPad(size);
     draw();
 }
+
+function randomize() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    let color = `rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
+
+randomizeBtn.addEventListener('click', 
+            () => {
+                    if (isRandomized) {
+                        isRandomized = false;
+                        randomizeBtn.textContent = "Randomize" 
+                    } 
+                    else {
+                        isRandomized = true;
+                        randomizeBtn.textContent = "Un-Randomize"
+                    }
+                  });
 
 newSketchBtn.addEventListener('click', newSketch);
 resetBtn.addEventListener('click', reset);
